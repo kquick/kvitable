10bf37b2d7173bc20158735390e5681aff995be9
Kevin Quick <kquick@galois.com>
2021-04-19 12:50:16 -0700

    Initial KVITable implementation, based on Data.Map

--- src/Data/KVITable.hs
+++ src/Data/KVITable.hs
@@ -1,12 +1,74 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Data.KVITable
   (
-    kvitable
+    KVITable(KVITable)
+  , Key
+  , KeyVal
+  , fromList
+  , toList
   )
 where
 
-import Data.Text ( Text )
+import           Data.Function ( on )
+import qualified Data.Map as Map
+import           Data.Text ( Text )
+import qualified GHC.Exts
 
-kvitable :: Text
-kvitable = "your table here"
+
+-- | The core KeyValue Indexed Table.  This table is similar to a Map,
+-- but the values are indexed by a list of Key+Value combinations, and
+-- the table contents can be sparse.
+
+data KVITable v = KVITable { contents     :: Map.Map [(Key, KeyVal)] v
+                           , valuecolName :: Text  -- ^ name of the value cells
+                           }
+
+instance Eq v => Eq (KVITable v) where
+  (==) = (==) `on` contents
+
+instance Show v => Show (KVITable v) where
+  show t = "KVITable { keyvals = " <> show (contents t) <>
+           ", valuecolName = " <> show (valuecolName t) <>
+           "}"
+
+type Key = Text
+type KeyVal = Text
+
+-- | The KVITable semigroup is left biased (same as Data.Map).
+instance Semigroup (KVITable v) where
+  a <> b = KVITable { contents = contents a <> contents b
+                    , valuecolName = valuecolName a
+                    }
+
+instance Monoid (KVITable v) where
+  mempty = KVITable { contents = mempty
+                    , valuecolName = "Value"
+                    }
+
+instance Functor KVITable where
+  fmap f t = KVITable { contents = fmap f (contents t)
+                      , valuecolName = valuecolName t
+                      }
+
+instance Foldable KVITable where
+  foldMap f = foldMap f . contents
+
+instance Traversable KVITable where
+  traverse f t = (\c -> KVITable { contents = c
+                                 , valuecolName = valuecolName t
+                                 }
+                 ) <$> traverse f (contents t)
+
+instance GHC.Exts.IsList (KVITable v) where
+  type Item (KVITable v) = ([(Key, KeyVal)], v)
+  fromList = (\c -> mempty { contents = c }) . GHC.Exts.fromList
+  toList = GHC.Exts.toList . contents
+
+
+fromList :: [ GHC.Exts.Item (KVITable v) ] -> KVITable v
+fromList = GHC.Exts.fromList
+
+toList :: KVITable v -> [ GHC.Exts.Item (KVITable v) ]
+toList = GHC.Exts.toList
--- test/TestMain.hs
+++ test/TestMain.hs
@@ -2,14 +2,35 @@
 
 module Main where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Data.List ( sort )
+import           Data.Text ( Text )
+import qualified Data.Text as T
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
-import Data.KVITable
+import           Data.KVITable
 
 main :: IO ()
 main = defaultMain $
-       testGroup "kvitable tests"
-       [
-         testCase "empty table" $ kvitable @=? "your table here"
-       ]
+       testGroup "kvitable tests" $
+       let listing1 :: [ ( [(Key, KeyVal)], Text ) ]
+           listing1 =
+             [ ( [("foo", "bar"), ("moo", "cow")], "one" )
+             , ( [("moo", "cow"), ("foo", "bar"), ("goose", "honk")], "two" )
+             , ( [("moo", "cow")], "three" )
+             , ( [("foo", "baz"), ("moo", "cow")], "four")
+             ]
+           kvi1 = fromList listing1
+       in
+         [
+           testCase "empty table" $ (mempty :: KVITable Bool) @=? (mempty :: KVITable Bool)
+
+         , testCase "to and from" $ sort listing1 @=? sort (toList kvi1)
+
+         , testCase "length" $ length listing1 @=? length kvi1
+
+         , testCase "mappable" $ sort ((fmap T.length) <$> listing1) @=? sort (toList (T.length <$> kvi1))
+
+         , testCase "traversable" $ Just (sort listing1) @=? sort . toList <$> (traverse Just kvi1)
+
+         ]
