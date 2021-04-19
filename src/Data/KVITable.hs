5aa45be50f4498e77835104afb3902b0c4022a46
Kevin Quick <kquick@galois.com>
2021-04-19 12:50:43 -0700

    Add keyVals, valueColName, insert, lookup, and rows  operations handling minimal and out-of-order keys.

--- src/Data/KVITable.hs
+++ src/Data/KVITable.hs
@@ -8,33 +8,55 @@ module Data.KVITable
   , KeyVal
   , fromList
   , toList
+  , Data.KVITable.lookup
+  , keyVals
+  , valueColName
+  , insert
+  , rows
   )
 where
 
 import           Data.Function ( on )
+import qualified Data.List as L
 import qualified Data.Map as Map
 import           Data.Text ( Text )
+import qualified Data.Text as T
 import qualified GHC.Exts
+import           Lens.Micro ( Lens' )
 
 
 -- | The core KeyValue Indexed Table.  This table is similar to a Map,
 -- but the values are indexed by a list of Key+Value combinations, and
 -- the table contents can be sparse.
 
-data KVITable v = KVITable { contents     :: Map.Map [(Key, KeyVal)] v
+data KVITable v = KVITable { keyvals      :: KeyVals -- ^ allowed value for keys (in order)
+                           , contents     :: Map.Map KeySpec v
+                             -- The invariant for the KVITable is that
+                             -- each KeySpec contains all keys listed
+                             -- in keyvals (in the same order) with
+                             -- the defaultKeyVal for any keys not
+                             -- explicitly provided for that value.
                            , valuecolName :: Text  -- ^ name of the value cells
                            }
 
 instance Eq v => Eq (KVITable v) where
+  -- n.b. keyvals (i.e. metadata) are _not_ used for equality, only contents
   (==) = (==) `on` contents
 
 instance Show v => Show (KVITable v) where
-  show t = "KVITable { keyvals = " <> show (contents t) <>
+  show t = "KVITable {" <>
+           " keyvals = " <> show (keyvals t) <>
+           " contents = " <> show (contents t) <>
            ", valuecolName = " <> show (valuecolName t) <>
            "}"
 
 type Key = Text
 type KeyVal = Text
+type KeySpec = [ (Key,  KeyVal ) ]
+type KeyVals = [ (Key, [KeyVal]) ]
+
+defaultKeyVal :: KeyVal
+defaultKeyVal = ""
 
 -- | The KVITable semigroup is left biased (same as Data.Map).
 instance Semigroup (KVITable v) where
@@ -43,12 +65,14 @@ instance Semigroup (KVITable v) where
                     }
 
 instance Monoid (KVITable v) where
-  mempty = KVITable { contents = mempty
+  mempty = KVITable { keyvals = mempty
+                    , contents = mempty
                     , valuecolName = "Value"
                     }
 
 instance Functor KVITable where
   fmap f t = KVITable { contents = fmap f (contents t)
+                      , keyvals = keyvals t
                       , valuecolName = valuecolName t
                       }
 
@@ -58,17 +82,140 @@ instance Foldable KVITable where
 instance Traversable KVITable where
   traverse f t = (\c -> KVITable { contents = c
                                  , valuecolName = valuecolName t
+                                 , keyvals = keyvals t
                                  }
                  ) <$> traverse f (contents t)
 
 instance GHC.Exts.IsList (KVITable v) where
-  type Item (KVITable v) = ([(Key, KeyVal)], v)
-  fromList = (\c -> mempty { contents = c }) . GHC.Exts.fromList
+  type Item (KVITable v) = (KeySpec, v)
+  fromList = foldr (\(ks,v) t -> insert ks v t) mempty
   toList = GHC.Exts.toList . contents
-
 
 fromList :: [ GHC.Exts.Item (KVITable v) ] -> KVITable v
 fromList = GHC.Exts.fromList
 
 toList :: KVITable v -> [ GHC.Exts.Item (KVITable v) ]
 toList = GHC.Exts.toList
+
+
+-- | Fetch or set the keyvals list via lenses. Note that setting the
+-- keyval list will drop any current contents in the table that do not
+-- have entries in the keyvals list.
+
+keyVals :: Lens' (KVITable v) KeyVals
+keyVals f t = (\kvs ->
+                 t { keyvals = kvs
+                   , contents =
+                     let inKVS spec _ = inkv spec kvs
+                         inkv [] [] = True
+                         inkv ((sk,sv):srs) ((k,vs):kv)
+                           | sk == k && sv `elem` vs = inkv srs kv
+                         inkv _ _ = False
+                     in Map.filterWithKey inKVS (contents t)
+                   }
+              ) <$> f (keyvals t)
+
+
+-- | Fetch or set the column name for the actual value cell in the
+-- KVITable.
+
+valueColName :: Lens' (KVITable v) Text
+valueColName f t = (\n -> t { valuecolName = n } ) <$> f (valuecolName t)
+
+
+-- | Retrieve an entry from the KVITable given a keyspec.  The keyspec
+-- may be minimally specified (i.e. it does not need to contain keys
+-- whose value is the default key value) and it may present the keys
+-- out of order and the lookup will still succeed (if there is a value
+-- for the normalized keyspec), but it will be faster to use the
+-- normalized key directly.
+
+lookup :: KeySpec -> KVITable v -> Maybe v
+lookup keyspec t = case Map.lookup keyspec $ contents t of
+                     Just v -> Just v
+                     Nothing ->
+                       -- keyspec might be under-specified or in a different order
+                       let ks = foldl keyandval [] (keyvals t)
+                           keyandval s (k,vs) = case L.lookup k keyspec of
+                             Just v -> if v `elem` vs
+                                       then s <> [(k,v)]
+                                       else [("Lookup key " <>
+                                               k <> " value of " <> v <>
+                                               " not in known values for that key: "
+                                             , T.pack $ show vs)]  -- bogus to fail the lookup below
+                             Nothing -> s <> [(k, defaultKeyVal)]
+                       in Map.lookup ks $ contents t
+
+-- | Inserts a new cell value into the table at the specified keyspec
+-- location.  The keyspec may be minimally specified and out-of-order.
+--
+-- This may be an expensive operation if it has to extend the keyvals
+-- for the table.  In general, insertion is expected to be less
+-- frequent than lookups so computation costs are biased towards the
+-- insertion operation.
+
+insert :: KeySpec -> v -> KVITable v -> KVITable v
+insert keyspec val t =
+  let remainingKeyValDefaults = fmap (\(k,_) -> (k, defaultKeyVal))
+      addDefVal e@(k,vs) = if defaultKeyVal `elem` vs then e else (k,vs <> [defaultKeyVal])
+      -- endset :: KeyVals -> KeySpec -> KeySpec -> KeyVals -> KVITable v
+      endset rkv [] tspec kvbld =
+        -- Reached the end of the user's keyspec but there are more
+        -- known keyvals in this KVITable, so add the entry with the
+        -- default KeyVal for the remaining keyspec (and ensure the
+        -- default KeyVal is listed in the table's keyvals).
+        let spec = tspec <> remainingKeyValDefaults rkv
+        in t { contents = Map.insert spec val (contents t)
+             , keyvals = kvbld <> (addDefVal <$> rkv)
+             }
+      endset [] spec tspec kvbld =
+        -- Reached the end of the known keyvals for this table but the
+        -- user's keyspec has additional elements.  This should extend
+        -- the tables keyvals with the remaining keyspec; also all
+        -- existing table values should be pushed out to use the
+        -- default values for the new keys in their keyspec.
+        let spec' = tspec <> spec
+            keyvals' = kvbld <> (fmap (:[]) <$> spec)
+            -- pushSpec = tspec <> remainingKeyValDefaults spec
+            -- pushKeyvals = kvbld <> (fmap (\v -> [v, defaultKeyVal]) <$> spec)
+            curTblList = Map.toList $ contents t
+            updTblList = fmap (\(ks,v) -> (ks <> remainingKeyValDefaults spec, v)) curTblList
+        in t { contents = Map.insert spec' val $ Map.fromList updTblList
+             , keyvals = keyvals'
+             }
+      endset kvs@((k,vs):rkvs) ((sk,sv):srs) tspec kvbld =
+        if k == sk
+        then let kv' = if sv `elem` vs
+                       then kvbld <> [(k,vs)]
+                       else kvbld <> [(k,vs <> [sv])]
+             in endset rkvs srs (tspec <> [(k,sv)]) kv'
+        else
+          -- re-arrange user spec crudely by throwing invalid
+          -- candidates to the end and retrying.  This isn't
+          -- necessarily efficient, but keyspecs aren't expected to be
+          -- longer than about a dozen entries.
+          if sk `elem` (fst <$> rkvs) && k `elem` (fst <$> srs)
+          then endset kvs (srs <> [(sk,sv)]) tspec kvbld
+          else
+            if any (`elem` (fst <$> kvs)) (fst <$> srs)
+            then endset kvs (srs <> [(sk,sv)]) tspec kvbld
+            else
+              let vs' = if defaultKeyVal `elem` vs then vs else (vs <> [defaultKeyVal])
+              in endset rkvs ((sk,sv):srs) (tspec <> [(k,defaultKeyVal)]) (kvbld <> [(k,vs')])
+  in endset (keyvals t) keyspec [] []
+
+
+-- | The 'rows' function returns a set of rows for the KVITable as a
+-- list structure, where each list entry is a different row.  A row
+-- consists of the /values/ of the keys for that row followed by the
+-- value of the entry (to get the names of the keys, use 'keyVals').
+
+rows :: KVITable v -> [ ([KeyVal], v) ]
+rows t = go (keyvals t) []
+  where
+    go [] spec = let spec' = reverse spec
+                 in case Map.lookup spec' (contents t) of
+                      Nothing -> []
+                      Just v -> [ (snd <$> spec', v) ]
+    go ((key, vals):kvs) spec =
+      concatMap (\v -> let spec' = (key,v):spec in go kvs spec') vals
--- test/TestMain.hs
+++ test/TestMain.hs
@@ -3,12 +3,18 @@
 module Main where
 
 import           Data.List ( sort )
+import           Data.Maybe ( catMaybes )
 import           Data.Text ( Text )
 import qualified Data.Text as T
+import           Lens.Micro ( (^.), (.~), (%~), (&) )
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 import           Data.KVITable
+
+import           Prelude hiding ( lookup )
+import           Debug.Trace
+
 
 main :: IO ()
 main = defaultMain $
@@ -20,17 +26,104 @@ main = defaultMain $
              , ( [("moo", "cow")], "three" )
              , ( [("foo", "baz"), ("moo", "cow")], "four")
              ]
+           kvi0 = mempty :: KVITable Text
            kvi1 = fromList listing1
        in
          [
            testCase "empty table" $ (mempty :: KVITable Bool) @=? (mempty :: KVITable Bool)
 
-         , testCase "to and from" $ sort listing1 @=? sort (toList kvi1)
+         , testCase "to and from" $ length listing1 @=? length (toList kvi1)
 
          , testCase "length" $ length listing1 @=? length kvi1
 
-         , testCase "mappable" $ sort ((fmap T.length) <$> listing1) @=? sort (toList (T.length <$> kvi1))
+         , testCase "mappable" $
+           sum (snd <$> (fmap T.length) <$> listing1) @=?
+           sum (snd <$> toList (T.length <$> kvi1))
 
-         , testCase "traversable" $ Just (sort listing1) @=? sort . toList <$> (traverse Just kvi1)
+         , testCase "traversable" $
+           Just (sort $ snd <$> listing1) @=?
+           sort . fmap snd . toList <$> (traverse Just kvi1)
 
+         , testCase "empty lookup fails" $
+           Nothing @=? lookup [("foo", "bar"), ("moo", "cow")] (mempty :: KVITable Bool)
+
+         , testCase "regular lookup" $
+           Just "one" @=? lookup [("foo", "bar"), ("moo", "cow")] kvi1
+
+         , testCase "add to empty table" $
+           let keyvals = [ ("foo", "bar"), ("moo", "cow") ]
+           in [ (keyvals, "hi") ] @=? toList (insert keyvals "hi" kvi0)
+
+         , testCase "getRows on empty table" $ [] @=? rows kvi0
+
+         , testCase "rows in simple table" $
+           let keyvals = [ ("foo", "bar"), ("moo", "cow") ]
+           in [ (["bar", "cow"], "hi") ] @=? rows (insert keyvals "hi" kvi0)
+
+         , testCaseSteps "non-leaf kvitable insert uses default key value" $ \step ->
+             do let keyvals = [ ("foo", "bar"), ("moo", "cow") ]
+                    t1 = insert keyvals "hi" kvi0
+
+                step "at start"
+                let t2 = insert [ ("moo", "dog") ] "oops" t1
+                rows t2 @?= [ ([ "bar", "cow" ], "hi")
+                            , ([ "",    "dog" ], "oops")
+                            ]
+
+                step "at end"
+                let t3 = insert [ ("foo", "dog") ] "oops" t1
+                rows t3 @?= [ ([ "bar", "cow" ], "hi")
+                            , ([ "dog", ""    ], "oops")
+                            ]
+
+                step "in middle"
+                let keyvals' = [ ("foo", "bar"), ("moo", "cow"), ("oink", "pig") ]
+                    t1' = insert keyvals' "hi" kvi0
+                    t4 = insert [ ("foo", "baz"), ("oink", "hog") ] "oops" t1'
+                rows t4 @?= [ ([ "bar", "cow", "pig" ], "hi")
+                            , ([ "baz", "",    "hog" ], "oops")
+                            ]
+
+         , testCaseSteps "valueColName" $ \step ->
+             do step "fetch"
+                "Value" @=? kvi1 ^. valueColName
+                step "set"
+                "says" @=? (kvi1 & valueColName .~ "says") ^. valueColName
+                step "update"
+                "says Value" @=? (kvi1 & valueColName %~ ("says " <>)) ^. valueColName
+
+         , testCase "keyVals fetch" $ [ ("foo", ["baz", "", "bar"])
+                                      , ("moo", ["cow"])
+                                      , ("goose", ["honk", ""])
+                                      ] @=? kvi1 ^. keyVals
+
+         , testCaseSteps "lookup" $ \step ->
+             do step "valid #1"
+                Just "three" @=? lookup [("moo", "cow")] kvi1
+                step "valid #2"
+                Just "two"   @=? lookup [("goose", "honk"), ("moo", "cow"), ("foo", "bar")] kvi1
+                step "valid #3"
+                Just "four"  @=? lookup [("moo", "cow"), ("foo", "baz")] kvi1
+                step "valid #4"
+                Just "one"   @=? lookup [("moo", "cow"), ("foo", "bar")] kvi1
+                step "valid with dups"
+                -- Note: ok to duplicate key values with identical entries
+                Just "one"   @=? lookup [("moo", "cow"), ("foo", "bar"), ("moo", "cow")] kvi1
+                step "invalid #1"
+                Nothing @=? lookup [("moo", "bar")] kvi1
+                step "invalid #2"
+                Nothing @=? lookup [("foo", "moo"), ("cow", "moo")] kvi1
+                step "invalid #3"
+                Nothing @=? lookup [] kvi1
+
+         , testCase "multiply-specified lookups" $
+             -- one of these will work, one will fail, but it's
+             -- indeterminate which one.  This is not a recommended
+             -- usage, but rather than waste computational resources
+             -- to prevent it, this is simply documenting this as a
+             -- known behavior and users are discouraged from using
+             -- it.
+             1 @=? (length $ catMaybes [ lookup [("moo", "sheep"), ("foo", "bar"), ("moo", "cow")] kvi1
+                                       , lookup [("moo", "cow"), ("foo", "bar"), ("moo", "sheep")] kvi1
+                                       ])
          ]
